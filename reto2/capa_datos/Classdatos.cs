58041b7fabbffbf21950c455d1be1ebe9d1c30f3
jaimepineros122 <98542927+jaimepineros122@users.noreply.github.com>
2022-04-17 23:49:52 -0500

    Add files via upload

---
+++ reto2/capa_datos/Classdatos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using capa_entidad;
+namespace capa_datos
+{
+    public class Classdatos
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["sql"].ConnectionString);
+
+        public DataTable D_lista_libros()
+        {
+            SqlCommand cmd = new SqlCommand("sp_libros", cn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public DataTable D_buscar_libro(Classentidad obje)
+        {
+            SqlCommand cmd = new SqlCommand("sp_buscar_libro", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@titulo", obje.titulo);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+        public string D_mantenimiento_libros(Classentidad obje)
+        {
+            string accion = "";
+            SqlCommand cmd = new SqlCommand("sp_grabar_libro", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@codigo", obje.codigo);
+            cmd.Parameters.AddWithValue("@titulo", obje.titulo);
+            cmd.Parameters.AddWithValue("@autor", obje.autor);
+            cmd.Parameters.AddWithValue("@editorial", obje.editorial);
+            cmd.Parameters.AddWithValue("@precio", obje.precio);
+            cmd.Parameters.AddWithValue("@cantidad", obje.cantidad);
+            cmd.Parameters.Add("@accion", SqlDbType.VarChar, 50).Value = obje.accion;
+            cmd.Parameters["@accion"].Direction = ParameterDirection.InputOutput;
+            if (cn.State == ConnectionState.Open) cn.Close();
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            accion = cmd.Parameters["@accion"].Value.ToString();
+            cn.Close();
+            return accion;
+
+        }
+
+    }
+}
